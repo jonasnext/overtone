92e8e55d91f37d976ab67aeef67d09db377ca29e
jonas <jonasnext@users.noreply.github.com>
2020-12-31 00:23:28 +0100

    project clojure

---
+++ project.clj
@@ -0,0 +1,8 @@
+(defproject untitled "0.1.0"
+  :description "chill vibes"
+  :url "https://github.com/jonasnext/overtone"
+  :license {:name "Apache License 2.0"
+            :url "http://www.apache.org/licenses/LICENSE-2.0"}
+  :dependencies [[org.clojure/clojure "1.10.1"]
+                 [overtone "0.10.6"]]
+  :repl-options {:init-ns untitled.core})
